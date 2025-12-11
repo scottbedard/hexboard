421c98a41bef6307ae6c8222ca8c98bd8c37df0a
Scott Bedard <scottbedard90@gmail.com>
2025-12-10 19:22:41 -0600

    add sandbox build

---
+++ vite.sandbox.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    vue(),
+  ],
+  build: {
+    outDir: 'dist-sandbox',
+  },
+})
+
