21f9821856de803936ec65c8dace45615d3e9982
Scott Bedard <scottbedard90@gmail.com>
2025-12-11 23:11:01 -0600

    wip

--- src/sandbox/App.vue
+++ src/sandbox/App.vue
@@ -9,10 +9,27 @@
       :playing="true"
       @move="onMove"
     />
+
+    <div class="flex gap-2">
+      <button
+        class="bg-gray-200 p-2 rounded"
+        @click="hapticConfirm"
+      >
+        positive
+      </button>
+
+      <button
+        class="bg-gray-200 p-2 rounded"
+        @click="hapticConfirm"
+      >
+        negative
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { hapticConfirm } from 'lib/components/hexboard/haptics'
 import { Hexboard } from 'lib/index'
 import { Hexchess, type San } from '@bedard/hexchess'
 import { ref } from 'vue'
