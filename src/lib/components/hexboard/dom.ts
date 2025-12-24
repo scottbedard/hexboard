9a8b089e1f83ec55e135455a1b20ee26588ba9be
Scott Bedard <scottbedard90@gmail.com>
2025-12-23 20:05:42 -0600

    round to fixed precision for safer ssr hydration

--- src/lib/components/hexboard/dom.ts
+++ src/lib/components/hexboard/dom.ts
@@ -9,11 +9,16 @@ export function d(arr: Vec<2>[]) {
 
   const [origin, ...points] = arr
 
-  let path = `M ${x(origin[0])} ${y(origin[1])} L `
+  let path = `M ${rounded(x(origin[0]))} ${rounded(y(origin[1]))} L `
 
   for (const point of points) {
-    path += `${x(point[0])} ${y(point[1])} `
+    path += `${rounded(x(point[0]))} ${rounded(y(point[1]))} `
   }
 
   return `${path} Z`
+}
+
+/** round a number to 5 decimal places */
+export function rounded(x: number) {
+  return x.toFixed(8)
 }
