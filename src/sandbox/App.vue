c48b2b4a10139aa3e6f6e52699e54a8f8fe97157
Scott Bedard <scottbedard90@gmail.com>
2025-12-23 18:59:33 -0600

    add promotion ui

--- src/sandbox/App.vue
+++ src/sandbox/App.vue
@@ -10,7 +10,48 @@
       :hexchess
       :playing="true"
       @move="onMove"
-    />
+    >
+      <template #promotion="{ b, n, q, r, promote }">
+        <div class="absolute bg-gray-50 cursor-pointer flex left-1/2 bottom-full rounded-md shadow-md overflow-hidden -translate-x-1/2">
+          <button
+            class="aspect-square border-r border-gray-200 cursor-pointer flex items-center justify-center size-12 hover:bg-gray-200"
+            @click="promote('b')"
+          >
+            <Component
+              class="size-3/4"
+              :is="b"
+            />
+          </button>
+          <button
+            class="aspect-square border-r border-gray-200 cursor-pointer flex items-center justify-center size-12 hover:bg-gray-200"
+            @click="promote('n')"
+          >
+            <Component
+              class="size-3/4"
+              :is="n"
+            />
+          </button>
+          <button
+            class="aspect-square border-r border-gray-200 cursor-pointer flex items-center justify-center size-12 hover:bg-gray-200"
+            @click="promote('r')"
+          >
+            <Component
+              class="size-3/4"
+              :is="r"
+            />
+          </button>
+          <button
+            class="aspect-square cursor-pointer flex items-center justify-center size-12 hover:bg-gray-200"
+            @click="promote('q')"
+          >
+            <Component
+              class="size-3/4"
+              :is="q"
+            />
+          </button>
+        </div>
+      </template>
+    </Hexboard>
   </div>
 </template>
 
